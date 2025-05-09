e195186215ccee533a9cba5341d9761aee352386
rjb <you@example.com>
2025-05-09 04:55:11 +0100

    commit

--- TestPW/obj/Debug/net8.0/TestPW.AssemblyInfo.cs
+++ TestPW/obj/Debug/net8.0/TestPW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6a1686db0758335d2bff9f72ca29b98b8fb8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
